0cf03e2d0dca6bbe419ff6f369c764256861c558
Christy <christymadden80@gmail.com>
2016-10-27 21:09:15 +0100

    Added new file types that can be selected for photos

--- SentenceBuilderPage.xaml.cs
+++ SentenceBuilderPage.xaml.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Pickers.Provider;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,8 +33,8 @@ namespace AutismCommunicationApp
             this.InitializeComponent();
         }
 
-        // Adapted from https://code.msdn.microsoft.com/windowsapps/How-to-upload-an-image-to-3293e4a8/sourcecode?fileId=153875&pathId=2041250276
-        private async void UploadButton_Click(object sender, RoutedEventArgs e)
+        // Copyright of https://code.msdn.microsoft.com/windowsapps/How-to-upload-an-image-to-3293e4a8/sourcecode?fileId=153875&pathId=2041250276
+        async void UploadButton_Click(object sender, RoutedEventArgs e)
         {
             var picker = new Windows.Storage.Pickers.FileOpenPicker();
             picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
@@ -55,9 +60,103 @@ namespace AutismCommunicationApp
                     BitmapImage bitmapImage = new BitmapImage();
                     bitmapImage.SetSource(fileStream);
                     uploadedImage.Source = bitmapImage;
+
+                    // Save the image here
+                    // Have a new button for saving
+                    FileSavePicker fileSavePicker = new FileSavePicker();
+                    fileSavePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                    //fileSavePicker.FileTypeChoices.Add("JPEG files", new List<string>() { ".jpg", ".jpeg", ".png" });
+                    fileSavePicker.FileTypeChoices.Add("PNG files", new List<string>() { ".png" });
+                    fileSavePicker.SuggestedFileName = "image";
+
+                    var outputFile = await fileSavePicker.PickSaveFileAsync();
+
+                    if (outputFile == null)
+                    {
+                        // The user cancelled the picking operation
+                        return;
+                    }
                 }
             }
-        }
 
-    }
-}
+
+
+            /*FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".jpg");
+            fileOpenPicker.FileTypeFilter.Add(".jpeg");
+            fileOpenPicker.FileTypeFilter.Add(".png");
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            var inputFile = await fileOpenPicker.PickSingleFileAsync();
+
+            if (inputFile == null)
+            {
+                // The user cancelled the picking operation
+                return;
+            }
+
+            SoftwareBitmap softwareBitmap;
+
+            using (IRandomAccessStream stream = await inputFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                // Get the SoftwareBitmap representation of the file
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            }*/ 
+
+        }// End method UploadButton_Click
+
+        
+        /*
+        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // Create an encoder with the desired format
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+                // Set the software bitmap
+                encoder.SetSoftwareBitmap(softwareBitmap);
+
+                // Set additional encoding parameters, if needed
+                encoder.BitmapTransform.ScaledWidth = 320;
+                encoder.BitmapTransform.ScaledHeight = 240;
+                encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    switch (err.HResult)
+                    {
+                        case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
+                                                         // If the encoder does not support writing a thumbnail, then try again
+                                                         // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw err;
+                    }
+                }
+
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
+
+
+            }// End using
+
+        }// End method SaveSoftwareBitmapToFile
+        */
+    }// End class
+  
+}// End namespace
