a090c3ebb9a891f1ee25eccdee7bc17e48ae468c
Christy <christymadden80@gmail.com>
2016-11-07 12:00:22 +0000

    Added basic model for pictures and their label

---
+++ DataModel/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace AutismCommunicationApp.DataModel
+{
+    class Picture
+    {
+
+        public BitmapImage picture { get; set; }
+        public string pictureLabel { get; set; }
+
+    }// End class
+}
