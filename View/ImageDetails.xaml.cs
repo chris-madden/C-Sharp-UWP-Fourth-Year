2dd1d8dafb6c994f718c0eaef6eebe6592a73a4d
Christy <christymadden80@gmail.com>
2016-11-12 14:28:29 +0000

    Can add label and image path to database

--- View/ImageDetails.xaml.cs
+++ View/ImageDetails.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutismCommunicationApp.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Storage;
@@ -6,14 +7,14 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace AutismCommunicationApp
 {
 
     // This page will display the selected image and allow user to give the image a label
     public sealed partial class ImageDetails : Page
     {
+
+        private string imagePath;
        
         public ImageDetails()
         {
@@ -35,6 +36,10 @@ namespace AutismCommunicationApp
 
             if (foundFile != null)
             {
+
+                // Store the path of the image in the local folder
+                imagePath = foundFile.Path;
+
                 // Convert file to a bitmap image
                 BitmapImage bp = await ImageUtils.StorageFileToBitmapImage(foundFile);
                 // NEED TO SHOW IMAGE TO XAML
@@ -43,7 +48,6 @@ namespace AutismCommunicationApp
 
         }// End OnNavigatedTo
 
-       
         // This method searches the devices local folder for the required image and if image is found it is 
         // returned as a storage file
         private async Task<StorageFile> SearchForFile(string imageName)
@@ -77,6 +81,40 @@ namespace AutismCommunicationApp
             return null;
 
         }// End method SearchForFile
+
+        // This method saves the image path and its label to the database
+        private void SaveToDB_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+            // Get text from label textbox
+            string label = Label.Text;
+
+            // Adapted from https://msdn.microsoft.com/en-us/library/system.string.isnullorempty(v=vs.110).aspx
+
+            // If the label textbox has text in it 
+            if (!String.IsNullOrEmpty(label))
+            {
+
+                // ==========  SAVE TO DATABASE  ==========
+
+                // Get access to the database
+                using (var db = new PictureContext())
+                {
+
+                    // Store image path and label into model
+                    var picture = new Picture { picturePath = imagePath, pictureLabel = label };
+
+                    // Add picture to table
+                    db.Pictures.Add(picture);
+
+                    // Save the database with the new picture
+                    db.SaveChangesAsync();
+
+                }// End using
+
+            }// End if
+
+        }// End SaveToDB_Click
 
     }// End class ImageDetails
 
